c7a98dcf63a5fadb271f31ef84ba36c984a5848e
Nihita Lele <124602601+NihitaLele@users.noreply.github.com>
2024-02-28 23:30:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-//your JS code here. If required.
+//Initialize two variables, highest and secondHighest, with -Infinity.
+// Iterate over the array. For each element:
+// If it’s greater than highest, update secondHighest with the value of highest, and update highest with the value of the current element.
+// If it’s less than highest but greater than secondHighest, update secondHighest with the value of the current element.
+// After the loop, return secondHighest
